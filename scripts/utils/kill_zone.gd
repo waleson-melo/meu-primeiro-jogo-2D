e285907eff58a73b1bedd283bae1be98bf78d646
Waleson Melo <walesonmelo@bemol.com.br>
2024-12-01 10:21:17 -0400

    subir

--- scripts/utils/kill_zone.gd
+++ scripts/utils/kill_zone.gd
@@ -3,9 +3,14 @@ extends Area2D
 @export var _timer: Timer = null
 
 
-func _on_body_entered(body: CharacterBody2D) -> void:
+func _on_body_entered(_body: CharacterBody2D) -> void:
 	_timer.start()
+	_body.dead()
+	#_body.velocity.y = -150
+	#Engine.time_scale = 0.5
+	#_body.get_node("Collision").queue_free()
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
